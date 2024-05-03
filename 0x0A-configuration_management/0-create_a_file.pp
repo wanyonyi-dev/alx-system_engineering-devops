608b81f9068a08f75cfb3b7ce5bb4c509aa5dbe5
wanyonyi-dev <wanyonyip148@gmail.com>
2024-05-03 10:31:55 -0700

    task 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/pup
+# creating a file named  /tmp/school with some content
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
